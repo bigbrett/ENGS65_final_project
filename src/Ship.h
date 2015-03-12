c0eaaf576836ba2dc02af8d6f1431721ace196c8
Brett Nicholas - MacbookPro <Brett.R.Nicholas.15@Dartmouth.edu>
2015-03-12 16:32:48 -0400

    graphics stuff!

--- src/Ship.h
+++ src/Ship.h
@@ -11,39 +11,10 @@
 #ifndef SHIP_H_
 #define SHIP_H_
 
-#include "GameObject.h"
-#include "Bullet.h"
-
-class Ship: public GameObject
-{
-private:
-	static SDL_Surface *image;	// The image used for the class
-	int shield;					// Damage reduction from collisions
-
-public:
-	Ship();
-	virtual ~Ship();
-
-	// Overloaded functions
-    void move();
-	void takeDmg(int dmg, Asteroid_GameObject_Type type);
-    list<GameObject*> destroy();
-
-	// Shoot function
-	// Returns a new Bullet object located at the front of the ship,
-    // traveling in the direction the ship is pointing
-	Bullet shoot();
-    
-    // Rotate 'increment' left (negative) or right (positive)
-    void rotate(int increment);
-    
-    // Accelerate
-    void accelerate();
-
-};
-
-
-
+#include <SDL2/SDL.h> 
+#include <SDL2_image/SDL_image.h> 
+#include <stdio.h>
+#include "Texture.h"
 
 //The dot that will move around on the screen
 class Dot
