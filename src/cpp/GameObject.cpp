a1e49db8658f6c322cc37dbfda53361c61c9e4ec
Dan Berman <daniel.m.berman.14@dartmouth.edu>
2015-03-14 16:41:24 -0400

    Revert 7bc4dbb..d24eb4a
    This rolls back to commit 7bc4dbbe58f4e193b29a505edfcb9a674eadb72c.

--- src/cpp/GameObject.cpp
+++ src/cpp/GameObject.cpp
@@ -63,12 +63,10 @@ void GameObject::move()
 
 void GameObject::collision(GameObject *obj1, GameObject *obj2)
 {
-    SDL_Rect *rect = new SDL_Rect;
     if(SDL_IntersectRect(&(obj1->collision_rect), &(obj2->collision_rect),
-                         rect))
+                         nullptr))
     {
         obj1->takeDmg(obj2->getDamage(), obj2->getObjectType());
         obj2->takeDmg(obj1->getDamage(), obj1->getObjectType());
     }
-    delete rect;
 }
